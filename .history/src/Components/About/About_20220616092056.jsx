104235750a55d57c30ac8b35ceac51e301b1425e
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-06-19 12:04:12 +0200

    Add about us

--- .history/src/Components/About/About_20220616092056.jsx
+++ .history/src/Components/About/About_20220616092056.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import '../../styles/About.module.css'
 
 
 const About = () => {
@@ -13,6 +14,13 @@ const About = () => {
    
     <p className="mb-3">Your gateway to environment tourism</p>
     <button className="bg-[#418D89] w-24 h-8 shadow-md rounded flex justify-center items-center  "><a className="" href="">Join us</a> </button>
+    </div>
+    
+
+    <div className="px-12 ">
+    <h1 className=" text-[38px] w-5/6 md:text-[45px] md:w-1/3 lg:text-[45px] lg:w-1/5 xl:w-1/6 sm:text-[45px] sm:w-1/6 leading-tight ">{"    "}About Us</h1>
+    <hr className="text-[#418D89] h-1.5 w-20 bg-[#418D89] rounded" />
+    <p className="mb-3 ">Welcome to Global Green Visa, a gateway to environment tourism. Working closely with national and international partners, GGV assists governments to deliver the environmental and climate benefits promised in the Paris Accords</p>
     </div>
     
 
